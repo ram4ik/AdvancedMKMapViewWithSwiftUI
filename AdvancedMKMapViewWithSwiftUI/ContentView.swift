3d622c96c39695d619d2a0ebad4a6349e0898ab8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-02 22:51:58 +0200

    Advanced MKMapView with SwiftUI - Bucket List SwiftUI

--- AdvancedMKMapViewWithSwiftUI/ContentView.swift
+++ AdvancedMKMapViewWithSwiftUI/ContentView.swift
@@ -6,11 +6,42 @@
 //  Copyright © 2019 Ramill Ibragimov. All rights reserved.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var location = [MKPointAnnotation]()
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: location)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.location.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
     }
 }
 
